30aef925bccb15278bbe55c305b3228fd13dcc9d
stylismo <j.e.schonewille@gmail.com>
2017-02-22 23:44:55 +0100

    #3 Don't suggest 'default annotation' for method which overrides method in superclass
    When a method overrides a method in a superclass the annotation of the corresponding parameter in the superclass applies.

--- src/main/java/com/stylismo/intellij/inspection/NullabilityAnnotationsWithTypeQualifierDefault.java
+++ src/main/java/com/stylismo/intellij/inspection/NullabilityAnnotationsWithTypeQualifierDefault.java
@@ -9,25 +9,33 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiJavaCodeReferenceElement;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifierList;
 import com.intellij.psi.PsiModifierListOwner;
+import com.intellij.psi.PsiParameter;
+import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import static com.intellij.codeInsight.AnnotationTargetUtil.extractRequiredAnnotationTargets;
 import static com.intellij.codeInsight.AnnotationTargetUtil.getTargetsForLocation;
 
-public class NullabilityAnnotationsWithTypeQualifierDefault {
+class NullabilityAnnotationsWithTypeQualifierDefault {
     private static final String TYPE_QUALIFIER_DEFAULT = "javax.annotation.meta.TypeQualifierDefault";
     private static final String JAVAX_ANNOTATION_NULLABLE = "javax.annotation.Nullable";
     private static final String JAVAX_ANNOTATION_NONNULL = "javax.annotation.Nonnull";
 
-    public static List<String> findAnnotations(@NotNull PsiModifierListOwner element, boolean nullable) {
+    static List<String> findAnnotations(@NotNull PsiModifierListOwner element, boolean nullable) {
+        if (overridesSuper(element)) {
+            return Collections.emptyList();
+        }
+
         List<String> annotations = new ArrayList<>();
         Project project = element.getProject();
         PsiModifierList modifierList = element.getModifierList();
@@ -43,6 +51,24 @@ public class NullabilityAnnotationsWithTypeQualifierDefault {
             }
         }
         return annotations;
+    }
+
+    private static boolean overridesSuper(PsiModifierListOwner element) {
+        if (element instanceof PsiParameter) {
+            PsiMethod method = (PsiMethod) element.getParent().getParent();
+            return !superMethods(method).isEmpty();
+        } else if (element instanceof PsiMethod) {
+            return !superMethods((PsiMethod) element).isEmpty();
+        }
+        return false;
+    }
+
+    private static List<MethodSignatureBackedByPsiMethod> superMethods(PsiMethod method) {
+        List<MethodSignatureBackedByPsiMethod> signatures = method.findSuperMethodSignaturesIncludingStatic(true);
+        signatures.removeIf(superSignature ->
+                superSignature.getMethod().getParameterList().getParametersCount()
+                        != method.getParameterList().getParametersCount());
+        return signatures;
     }
 
     private static boolean isNullabilityAnnotationForTypeQualifierDefault(@NotNull PsiAnnotation annotation,
